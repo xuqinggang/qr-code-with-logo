ff0e5aa378bfcc1329a34bda8bd791d9fd4772b0
xuqinggang <xuqinggang@bytedance.com>
2018-10-21 14:42:43 +0800

    fix:rollup to support browser and node two environment builds

---
+++ rollup.browser.config.js
@@ -0,0 +1,64 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace';
+
+const watch = {
+  include: 'src/**',
+  exclude: 'node_modules/**'
+}
+
+export default [
+  // browser build
+  {
+    input: 'src/browser.js',
+
+    output: [
+      {
+        format: 'cjs',
+        exports: 'named',
+        file: 'lib/browser.js'
+      },
+    ],
+
+    watch,
+
+    plugins: [
+      resolve({browser: true}),
+      commonjs(),
+      babel(),
+      replace({
+        RUN_ENV: JSON.stringify(process.env.RUN_ENV || 'browser'),
+      }),
+    ],
+
+    external: ['qrcode'],
+  },
+  // browser build uglify
+  {
+    input: 'src/browser.js',
+    output: [
+      {
+        format: 'umd',
+        file: 'lib/qr-code-with-logo.browser.min.js',
+        name: 'QrCodeWithLogo',
+        globals: {
+          'QrCodeWithLogo': 'QrCodeWithLogo',
+        },
+      },
+    ],
+
+    watch,
+
+    plugins: [
+      resolve({browser: true}),
+      commonjs(),
+      babel(),
+      replace({
+        RUN_ENV: JSON.stringify(process.env.RUN_ENV || 'browser'),
+      }),
+      uglify(),
+    ],
+  },
+]
--- rollup.config.js
+++ rollup.config.js
@@ -1,66 +1,40 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace';
 
 const watch = {
   include: 'src/**',
   exclude: 'node_modules/**'
 }
 
-export default [{
-  input: 'index.js',
+export default [
+  // server build
+  {
+    input: 'src/server.js',
 
-  output: [
-    {
-      format: 'cjs',
-      exports: 'named',
-      file: 'lib/index.js'
-    },
-    {
-      format: 'es',
-      file: 'lib/index.esm.js'
-    },
-    {
-      format: 'umd',
-      file: 'lib/index.umd.js',
-      name: 'QrCodeWithLogo',
-      globals: {
-        'qrcode': 'QRCode',
-        'QrCodeWithLogo': 'QrCodeWithLogo'
-      }
-    }
-  ],
+    output: [
+      {
+        format: 'cjs',
+        exports: 'named',
+        file: 'lib/index.js'
+      },
+      {
+        format: 'es',
+        file: 'lib/index.esm.js'
+      },
+    ],
 
-  watch,
+    watch,
 
-  plugins: [
-    babel()
-  ],
-
-  external: ['qrcode']
-}, {
-  input: 'index.js',
-
-  output: [
-    {
-      format: 'umd',
-      file: 'lib/qr-code-with-logo.browser.min.js',
-      name: 'QrCodeWithLogo',
-      globals: {
-        'QrCodeWithLogo': 'QrCodeWithLogo'
-      }
-    }
-  ],
-
-  watch,
-
-  plugins: [
-    resolve({browser: true}),
-    commonjs(),
-    babel(),
-    uglify()
-  ],
-
-  external: []
-}]
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel(),
+      replace({
+        RUN_ENV: JSON.stringify(process.env.RUN_ENV || 'browser'),
+      }),
+    ],
+    external: ['qrcode', 'canvas']
+  },
+];
